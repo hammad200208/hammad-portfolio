82b59f7b57baa54a62b15030d0133948e2d6d062
Hammad Khan <hamdev73@gmail.com>
2025-07-17 16:20:23 +0500

    install icons package

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { FaLinkedin, FaGithub, FaBriefcase } from "react-icons/fa";
-import { motion } from "framer-motion";
+import {motion} from "framer-motion";
 
 const Home = () => {
   return (
